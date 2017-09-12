a10ae7b143ca2f65d20f5e88d0ab46e9446f7e56
clicksun <clicksun@gmail.com>
2017-09-12 17:11:42 -0500

    typo fixed

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Order = db.define('Order', {
+const Order = db.define('order', {
   status: {
     type: Sequelize.ENUM,
     values: ['created', 'processing', 'cancelled', 'completed'],

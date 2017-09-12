ef2326e900617c6b0a94b93f7ad69beb1d3de732
Joon Kim <joon3575@gmail.com>
2017-09-12 15:31:36 -0500

    Category model completed

--- client/history.js
+++ client/history.js
@@ -4,3 +4,8 @@ import createMemoryHistory from 'history/createMemoryHistory'
 const history = process.env.NODE_ENV === 'test' ? createMemoryHistory() : createHistory()
 
 export default history
+
+
+
+
+

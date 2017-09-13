85bb485c57a6bedc346a960d5383947fd03c8d07
clicksun <clicksun@gmail.com>
2017-09-13 16:37:16 -0500

    add product photos

--- seed.js
+++ seed.js
@@ -39,11 +39,6 @@ const categories = [
   { title: 'Drum' },
   { title: 'Bass' },
   { title: 'Bassoon' },
-  { title: 'Violin' },
-  { title: 'Ukulele' },
-  { title: 'Trumpet' },
-  { title: 'Cello' },
-  { title: 'Bagpipe' },
 ];
 
 const createCategories = (() => (
@@ -52,17 +47,79 @@ const createCategories = (() => (
   )))
 ));
 
+/* -----------  Set up Photo data ----------- */
+
+// TODO: add more images if you have free time!
+const guitarPhotos = [
+  [
+    { photoURL: 'https://www.taylorguitars.com/sites/default/files/styles/multi_column_guitar_three/public/responsive-guitar-detail/Taylor-524ce-fr-2016.png?itok=ENjzoiOl' },
+    { photoURL: 'http://jamestownarts.com/wp-content/uploads/2017/03/maxresdefault.jpg' },
+    { photoURL: 'https://i.pinimg.com/736x/24/ea/ff/24eaff9ac0e141a80437de1e3be78b6e--bass-guitars-acoustic-guitars.jpg' },
+  ],
+  [
+    { photoURL:
+      'https://s3-us-west-2.amazonaws.com/static.music-man.com/website/images/instruments/instrument-20.png?1467907743' },
+    { photoURL:
+      'http://www.guitarworld.com/sites/default/files/public/2016/06/BBPOP621.jpg' },
+    { photoURL:
+      'http://www.guitarworld.com/sites/default/files/public/2016/08/SteveVaiGuitars_4796.jpg' },
+  ],
+  [
+    { photoURL:
+      'https://www.taylorguitars.com/sites/default/files/styles/multi_column_guitar_three/public/responsive-guitar-detail/Taylor-814ce-fr-2015.png?itok=B3YG3tBf' },
+    { photoURL:
+      'http://media.guitarcenter.com/is/image/MMGS7/FA-135CE-Cutaway-Concert-Acoustic-Electric-Guitar-Natural/H70301000001000-00-500x500.jpg' },
+  ],
+];
+
+const keyboardPhotos = [
+  [
+    { photoURL: 'http://www.fairdealmusic.co.uk/media/catalog/product/cache/1/image/1ad2536b4e0b64b5213084f52c752e7f/c/t/ctk-7200-keyboard_4.jpg' },
+    { photoURL: 'http://musicappblog.com/wp-content/uploads/2013/07/alesis-Q25-keyboard-promo-shot.jpg' },
+    { photoURL: 'https://images-na.ssl-images-amazon.com/images/G/01/img16/musical-instruments/vertical-store/keyboards/mi_vertical_keyboards_short_2._CB278962455_.jpg' },
+  ],
+];
+
+const drumPhotos = [
+  [
+    { photoURL: 'http://thehub.musiciansfriend.com/images/drumsbuying/yamaha-stage-custom-drum-kit.jpg' },
+    { photoURL: 'https://media.sweetwater.com/api/i/ha-e7809fd91e0cc213__q-85__hmac-52a964bbf2ca74645de6a1f9a246b0c2b656b2a7/images/items/1800/LJR5PCWR-xlarge.jpg' },
+    { photoURL: 'https://i.ytimg.com/vi/CYNnqKA1AKo/maxresdefault.jpg' },
+  ],
+];
+
+const bassPhotos = [
+  [
+    { photoURL: 'http://www.deanguitars.com/images/productimages/e5fmtbks/e5fmtbks.png' },
+    { photoURL: 'http://www.deanguitars.com/images/productimages/e2vn/e2vn.png' },
+    { photoURL: 'https://i.ytimg.com/vi/9N1MPe383Bk/maxresdefault.jpg' },
+  ],
+];
+
+const bassoonPhotos = [
+  [
+    { photoURL: 'https://az58332.vo.msecnd.net/e88dd2e9fff747f090c792316c22131c/Images/Products1247-1200x1200-89950.jpg' },
+    { photoURL: 'https://static1.squarespace.com/static/53bf2b1ce4b0369fa55d0caf/t/5646ab60e4b0ac3d5288fbba/1447472208643/Bassoon' },
+  ],
+];
+
+const createPhotos = (() => (
+  Promise.all(photos.map(category => (
+    Category.create(category)
+  )))
+));
+
 /* -----------  Set up Product data ----------- */
 
 const numProducts = 10; // 10 instruments per category
 
-const randomProduct = (catTitle) => {
+const randomProduct = (category) => {
   let title = faker.commerce.productName();
 
   // this is to replace last word with category name
   const titleArr = title.split(' ');
   titleArr.pop();
-  titleArr.push(catTitle);
+  titleArr.push(category.title);
   title = titleArr.join(' ');
 
   return Product.create({
@@ -70,13 +127,14 @@ const randomProduct = (catTitle) => {
     description: faker.lorem.paragraph(),
     price: faker.commerce.price(),
   })
+    .then(product => category.addProduct(product))
     .catch(err => console.error(err));
 };
 
-const createProducts = (catTitle) => {
+const createProducts = (category) => {
   const promiseArr = [];
   for (let i = 0; i < numProducts; i += 1) {
-    promiseArr.push(randomProduct(catTitle));
+    promiseArr.push(randomProduct(category));
   }
   return Promise.all(promiseArr);
 };
@@ -85,7 +143,7 @@ const createProducts = (catTitle) => {
 const seed = (() => (
   createCategories()
     .then((cats) => {
-      const promiseArr = cats.map(category => createProducts(category.title));
+      const promiseArr = cats.map(category => createProducts(category));
       return Promise.all(promiseArr);
     })
     .then(() => createUsers())

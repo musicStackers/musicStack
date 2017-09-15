128ac4987d1c23450e32d051642a7f13d0c61648
Joon Kim <joon3575@gmail.com>
2017-09-14 20:07:02 -0500

    user-home test spec, auth-form fixed

--- client/components/auth-form.jsx
+++ client/components/auth-form.jsx
@@ -1,34 +1,34 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import PropTypes from 'prop-types'
-import {auth} from '../store'
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { auth } from '../store';
 
 /**
  * COMPONENT
  */
 const AuthForm = (props) => {
-  const {name, displayName, handleSubmit, error} = props
+  const { name, displayName, handleSubmit, error } = props;
 
   return (
     <div>
       <form onSubmit={handleSubmit} name={name}>
         <div>
-          <label htmlFor='email'><small>Email</small></label>
-          <input name='email' type='text' />
+          <label htmlFor="email"><small>Email</small></label>
+          <input name="email" type='text' />
         </div>
         <div>
-          <label htmlFor='password'><small>Password</small></label>
-          <input name='password' type='password' />
+          <label htmlFor="password"><small>Password</small></label>
+          <input name="password" type="password" />
         </div>
         <div>
-          <button type='submit'>{displayName}</button>
+          <button type="submit">{displayName}</button>
         </div>
         {error && error.response && <div> {error.response.data} </div>}
       </form>
-      <a href='/auth/google'>{displayName} with Google</a>
+      <a href="/auth/google">{displayName} with Google</a>
     </div>
-  )
-}
+  );
+};
 
 /**
  * CONTAINER
@@ -41,28 +41,28 @@ const mapLogin = (state) => {
   return {
     name: 'login',
     displayName: 'Login',
-    error: state.user.error
-  }
-}
+    error: state.user.error,
+  };
+};
 
 const mapSignup = (state) => {
   return {
     name: 'signup',
     displayName: 'Sign Up',
-    error: state.user.error
-  }
-}
+    error: state.user.error,
+  };
+};
 
 const mapDispatch = (dispatch) => {
   return {
-    handleSubmit (evt) {
-      evt.preventDefault()
-      const formName = evt.target.name
-      const email = evt.target.email.value
-      const password = evt.target.password.value
-      dispatch(auth(email, password, formName))
-    }
-  }
+    handleSubmit(evt) {
+      evt.preventDefault();
+      const formName = evt.target.name;
+      const email = evt.target.email.value;
+      const password = evt.target.password.value;
+      dispatch(auth(email, password, formName));
+    },
+  },
 }
 
 export const Login = connect(mapLogin, mapDispatch)(AuthForm)

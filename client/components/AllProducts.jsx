e567f05b8d2bb97354558ada0fd5e0cef2eb1f23
jannncodes <janineagarcia@gmail.com>
2017-09-14 21:50:49 -0500

    add styles for product page

--- client/components/AllProducts.jsx
+++ client/components/AllProducts.jsx
@@ -6,6 +6,7 @@ import styled from 'styled-components';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Checkbox from 'material-ui/Checkbox';
 import { H1, H2, H3, PhotoDivider, SideBar } from './reusableStyles';
+import ProductSnapshot from './ProductSnapshot.jsx';
 
 // Component
 function AllProducts() {
@@ -16,13 +17,22 @@ function AllProducts() {
     checkbox: {
       marginBottom: 16,
     },
+    input: {
+      width: 20,
+    },
   };
 
   const ProductsTitlePhoto = PhotoDivider.extend`
     background-image: url('http://via.placeholder.com/350x150');
   `;
 
-  const categories = [// bring in categories from mapState as Props
+  const ProductsWrapper = styled.div`
+    height:100%;
+    float: left;
+    border-left: 2px solid #69b6ff;
+  `;
+
+  const categories = [ // bring in categories from mapState as Props
     { id: 1, title: 'guitars', img: 'http://via.placeholder.com/350x150' },
     { id: 2, title: 'drums', img: 'http://via.placeholder.com/350x150' },
     { id: 3, title: 'saxophones', img: 'http://via.placeholder.com/350x150' },
@@ -30,6 +40,12 @@ function AllProducts() {
     { id: 5, title: 'synths', img: 'http://via.placeholder.com/350x150' },
     { id: 6, title: 'pianos', img: 'http://via.placeholder.com/350x150' },
     { id: 7, title: 'electronics', img: 'http://via.placeholder.com/350x150' },
+  ];
+
+  const products = [ // bring in 3 products from mapState as Props
+    { id: 1, title: 'best guitar', img: 'http://via.placeholder.com/350x150', price: 4, stars: 3, review: 'This guitar set is so awesome!!! It has everything for you to play immediately. ' },
+    { id: 2, title: 'best drums', img: 'http://via.placeholder.com/350x150', price: 4, stars: 3, review: 'This guitar set is so awesome!!! It has everything for you to play immediately. ' },
+    { id: 3, title: 'best flutes', img: 'http://via.placeholder.com/350x150', price: 4, stars: 3, review: 'This guitar set is so awesome!!! It has everything for you to play immediately. ' },
   ];
 
   // Styled Components
@@ -61,24 +77,54 @@ function AllProducts() {
               <H3>Min</H3>
               <TextField
                 name="minprice"
+                style={styles.input}
               />
             </div>
             <div>
               <H3>Min</H3>
               <TextField
                 name="maxprice"
+                style={styles.input}
               />
             </div>
           </div>
+          <div>
+            <H3>Stars</H3>
+          </div>
         </SideBar>
+        <ProductsWrapper>
+          {
+            products.map((product) => {
+              return (
+                <ProductSnapshot
+                  key={product.id}
+                  id={product.id}
+                  title={product.title}
+                  img={product.img}
+                  price={product.price}
+                  stars={product.stars}
+                  review={product.review}
+                />
+              );
+            })
+          }
+        </ProductsWrapper>
       </div>
     </MuiThemeProvider>
   );
 }
 
 // Container
-const mapState = null;
+// const mapState = (state, ownProps) => ({
+//   categories: state.categories,
+//   products: state.products,
+// });
 
+// const mapDispatch = (dispatch) => {
+//   return {
+//   };
+// };
+const mapState = null;
 const mapDispatch = null;
 
 export default connect(mapState, mapDispatch)(AllProducts);

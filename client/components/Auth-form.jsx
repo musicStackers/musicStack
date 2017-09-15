ef735139d17075a68611ac6b459795cd4f196c67
clicksun <clicksun@gmail.com>
2017-09-15 15:26:17 -0500

    Finish log in and sign up pages

--- client/components/Auth-form.jsx
+++ client/components/Auth-form.jsx
@@ -1,32 +1,87 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { auth } from '../store';
+import { Paper, TextField, RaisedButton, FlatButton } from 'material-ui';
+import Divider from 'material-ui/Divider';
+import ActionGoogle from 'material-ui/svg-icons/action/android';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { auth } from '../store/reducers/user';
+
+// Styles
+const styles = {
+  paper: {
+    height: 450,
+    width: 500,
+    margin: '100px auto',
+    textAlign: 'center',
+    display: 'block',
+    overflow: 'hidden',
+  },
+  divider: {
+    'margin-left': '40px',
+    'margin-right': '40px',
+  },
+  raisedButton: {
+    width: 180,
+    height: 40,
+    margin: '60px 0 30px 0',
+  },
+  flatButton: {
+    margin: '30px 0 30px 0',
+  },
+  input: {
+    width: 300,
+  },
+};
+
 
 /**
  * COMPONENT
  */
 const AuthForm = (props) => {
   const { name, displayName, handleSubmit, error } = props;
-
+  const googleLabel = `${displayName} with Google`;
   return (
-    <div>
-      <form onSubmit={handleSubmit} name={name}>
-        <div>
-          <label htmlFor="email"><small>Email</small></label>
-          <input name="email" type="text" />
-        </div>
-        <div>
-          <label htmlFor="password"><small>Password</small></label>
-          <input name="password" type="password" />
-        </div>
-        <div>
-          <button type="submit">{displayName}</button>
-        </div>
-        {error && error.response && <div> {error.response.data} </div>}
-      </form>
-      <a href="/auth/google">{displayName} with Google</a>
-    </div>
+    <MuiThemeProvider>
+      <Paper style={styles.paper} zDepth={2}>
+        <h2>{displayName}</h2>
+        <form onSubmit={handleSubmit} name={name}>
+          <div>
+            <TextField
+              name="email"
+              hintText="forte@forte.com"
+              floatingLabelText="Email"
+              style={styles.input}
+            />
+            <br />
+            <TextField
+              name="password"
+              hintText="must be at least 8 characters"
+              floatingLabelText="Password"
+              type="password"
+              style={styles.input}
+            />
+            <br />
+            <RaisedButton
+              label={displayName}
+              type="submit"
+              style={styles.raisedButton}
+            />
+            <br />
+            {error && error.response && <div> {error.response.data} </div>}
+          </div>
+        </form>
+        <Divider style={styles.divider} />
+        <FlatButton
+          href="/auth/google"
+          label={googleLabel}
+          primary={true}
+          icon={<ActionGoogle />}
+          style={styles.flatButton}
+        />
+      </Paper>
+    </MuiThemeProvider>
   );
 };
 

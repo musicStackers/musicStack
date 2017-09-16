de2ae3c2c08e6970ae0394eebf3d9983a3ea22a0
Joon Kim <joon3575@gmail.com>
2017-09-16 17:23:14 -0500

    Display faulty data

--- client/components/user-home.jsx
+++ client/components/user-home.jsx
@@ -1,17 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { H1, H2, H3, ImagesWrapper } from './reusableStyles';
+
 
 /**
  * COMPONENT
  */
-export const UserHome = (props) => {
-  const { email } = props;
 
+// const fakeUser = {
+//   id: 1,
+//   Email: 'janinegarcia@gmail.com',
+//   Address: 'Chicago, IL',
+// }
+
+// const orders = [
+//   { title: 'Bass' },
+//   { title: 'Guitar' },
+//   { title: 'Piano' },
+// ]
+
+export const UserHome = ({ email, address, orders }) => {
   return (
-    <div>
-      <h3>Welcome, {email}</h3>
-    </div>
+    <MuiThemeProvider>
+      <div>
+        <H1>Welcome!</H1>
+        <ImagesWrapper>
+          <div>
+            <H2>Personal Detail</H2>
+            <h5>Email: {users.email}</h5>
+            <h5>Address: {fakeUser.address}</h5>
+          </div>
+          <div>
+            <H2>Your Orders</H2>
+            {
+              orders.map((order) => {
+                return (
+                  <Link to={`/orders/${fakeUser.id}`} key={order.id}>
+                    <h5>{order.title}</h5>
+                  </Link>
+                );
+              })
+            }
+          </div>
+        </ImagesWrapper>
+      </div>
+    </MuiThemeProvider>
   );
 };
 
@@ -21,6 +57,8 @@ export const UserHome = (props) => {
 const mapState = (state) => {
   return {
     email: state.user.email,
+    address: state.user.address,
+    orders: state.orders,
   };
 };
 
@@ -30,6 +68,8 @@ export default connect(mapState)(UserHome);
  * PROP TYPES
  */
 UserHome.propTypes = {
-  email: PropTypes.string,
+  email: PropTypes.string.isRequired,
+  address: PropTypes.string.isRequired,
+  orders: PropTypes.string.isRequired,
 };
 
--- client/routes.jsx
+++ client/routes.jsx
@@ -4,7 +4,7 @@ import { Router } from 'react-router';
 import { Route, Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import history from './history';
-import { Nav, Landing, AllProducts, Dashboard, Product, Cart, Login, Signup, Checkout } from './components';
+import { Nav, Landing, AllProducts, Dashboard, Product, Cart, Login, Signup, Checkout, UserHome } from './components';
 import { me } from './store/reducers/user/';
 import { fetchCategories } from './store/reducers/categories';
 import { fetchCategoryProduct } from './store/reducers/category_product';
@@ -48,6 +48,7 @@ class Routes extends Component {
             <Route exact path="/login/" component={Login} />
             <Route exact path="/cart" component={Cart} />
             <Route exact path="/cart/checkout" component={Checkout} />
+            <Route exact path="/home" component={UserHome} />
             <Route component={Landing} />
           </Switch>
         </div>

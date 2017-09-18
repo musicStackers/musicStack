c4f2fd3f14ddc3a2f641753eb549b837987ff24c
jannncodes <janineagarcia@gmail.com>
2017-09-18 11:18:36 -0500

    fix

--- client/components/ReviewForm.jsx
+++ client/components/ReviewForm.jsx
@@ -29,31 +29,31 @@ function ReviewForm({ description, star, setReviewDescription, setReviewStar, cr
     },
   };
 
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <RadioButtonGroup name="star-review" onChange={handleRadioChange} >
-          <RadioButtonGroup
-            value="1"
-            checkedIcon={<ActionStar />}
-            uncheckedIcon={<ActionStarBorder />}
-            style={styles.radioButton}
-        </RadioButtonGroup>
-        <input name="stars" type="radio" value="1" id="star-1" checked={star === '1'} onChange={handleRadioChange} />
-        <label htmlFor="star-1">1</label>
-        <input name="stars" type="radio" value="2" id="star-2" checked={star === '2'} onChange={handleRadioChange} />
-        <label htmlFor="star-2">2</label>
-        <input name="stars" type="radio" value="3" id="star-3" checked={star === '3'} onChange={handleRadioChange} />
-        <label htmlFor="star-3">3</label>
-        <input name="stars" type="radio" value="4" id="star-4" checked={star === '4'} onChange={handleRadioChange} />
-        <label htmlFor="star-4">4</label>
-        <input name="stars" type="radio" value="5" id="star-5" checked={star === '5'} onChange={handleRadioChange} />
-        <label htmlFor="star-5">5</label>
-        <textarea name="description" value={description} onChange={e => setReviewDescription(e.target.value)} />
-        <input type="submit" value="Submit Review" />
-      </form>
-    </div>
-  );
+  return
+    // <div>
+    //   <form onSubmit={handleSubmit}>
+    //     <RadioButtonGroup name="star-review" onChange={handleRadioChange} >
+    //       <RadioButtonGroup
+    //         value="1"
+    //         checkedIcon={<ActionStar />}
+    //         uncheckedIcon={<ActionStarBorder />}
+    //         style={styles.radioButton}
+    //     </RadioButtonGroup>
+    //     <input name="stars" type="radio" value="1" id="star-1" checked={star === '1'} onChange={handleRadioChange} />
+    //     <label htmlFor="star-1">1</label>
+    //     <input name="stars" type="radio" value="2" id="star-2" checked={star === '2'} onChange={handleRadioChange} />
+    //     <label htmlFor="star-2">2</label>
+    //     <input name="stars" type="radio" value="3" id="star-3" checked={star === '3'} onChange={handleRadioChange} />
+    //     <label htmlFor="star-3">3</label>
+    //     <input name="stars" type="radio" value="4" id="star-4" checked={star === '4'} onChange={handleRadioChange} />
+    //     <label htmlFor="star-4">4</label>
+    //     <input name="stars" type="radio" value="5" id="star-5" checked={star === '5'} onChange={handleRadioChange} />
+    //     <label htmlFor="star-5">5</label>
+    //     <textarea name="description" value={description} onChange={e => setReviewDescription(e.target.value)} />
+    //     <input type="submit" value="Submit Review" />
+    //   </form>
+    // </div>
+  // );
 }
 
 const mapState = (state, ownProps) => ({

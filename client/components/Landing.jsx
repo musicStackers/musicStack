2adcb01eddfaef1585b7b750111df7300b079b64
clicksun <clicksun@gmail.com>
2017-09-18 14:17:00 -0500

    update to home style

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { GridList, GridTile } from 'material-ui/GridList';
-import { H1, PhotoDivider, ImagesWrapper } from './reusableStyles';
+import { H1, PhotoDivider, ImagesWrapper, PhotoH1 } from './reusableStyles';
 
 
 // Component
@@ -16,6 +16,9 @@ function Landing({ categories, picksProducts, picksPhotos }) {
       overflowX: 'auto',
       overflowY: 'auto',
     },
+    tileTitle: {
+      fontSize: 30,
+    },
   };
 
   const CatPhotoWrapper = styled.div`
@@ -24,24 +27,33 @@ function Landing({ categories, picksProducts, picksPhotos }) {
     overflow: hidden;
   `;
 
-  const CategoryPhotos = () =>
-    (<ImagesWrapper>
+  const PickPhotoWrapper = styled.div`
+    vertical-align: middle;
+    overflow: hidden;
+    margin: 0 auto;
+    height: auto;
+    border-radius: 50%;
+  `;
+
+  const CategoryPhotoGrid = () => (
+    <ImagesWrapper>
       <GridList
         style={styles.gridList}
-        cols={5}
-        padding={1}
+        padding={2}
         cellHeight={400}
       >
         <GridTile
           key={0}
           title="All Products"
           cols={2}
+          titleStyle={styles.tileTitle}
         >
           <CatPhotoWrapper>
             <Link to="/products">
               <img
-                src="https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg"
+                src="/assets/home-allproducts.jpg"
                 alt="All Products"
+                height="400px"
               />
             </Link>
           </CatPhotoWrapper>
@@ -49,15 +61,17 @@ function Landing({ categories, picksProducts, picksPhotos }) {
         {
           categories.map(category => (
             <GridTile
+              style={styles.gridList}
               key={category.id}
               title={category.title}
-              cols={1}
+              titleStyle={styles.tileTitle}
             >
               <CatPhotoWrapper>
                 <Link to={`/categories/${category.id}`}>
                   <img
-                    src="https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg"
-                    alt="All Products"
+                    src={`/assets/home-${category.title.toLowerCase()}.jpg`}
+                    alt={category.title}
+                    height="400px"
                   />
                 </Link>
               </CatPhotoWrapper>
@@ -65,38 +79,60 @@ function Landing({ categories, picksProducts, picksPhotos }) {
           ))
         }
       </GridList>
-    </ImagesWrapper>);
+    </ImagesWrapper>
+  );
 
   const OurPicksDivider = PhotoDivider.extend`
     height: 200px;
     background-image: url('/assets/allproductsheader.jpg');
   `;
 
+  const PickedProuctGrid = (() => (
+    <div className="our-picks">
+      <ImagesWrapper>
+        <GridList
+          style={styles.gridList}
+          cols={3}
+          padding={2}
+          cellHeight={300}
+        >
+          {
+            picksProducts.map((product, index) => {
+              const photo = picksPhotos.find(p => +p.productId === +product.id);
+              return (
+                <GridTile
+                  key={product.id}
+                  title={product.title}
+                  cols={1}
+                >
+                  <PickPhotoWrapper>
+                    <Link to={`/product/${product.id}`} key={product.id}>
+                      <img
+                        src={photo && photo.photoURL}
+                        alt={photo && photo.title}
+                        height="300px"
+                        width="300px"
+                      />
+                    </Link>
+                  </PickPhotoWrapper>
+                </GridTile>
+              );
+            })
+          }
+        </GridList>
+      </ImagesWrapper>
+    </div>
+  ));
+
   // Styled Components
   return (
     <MuiThemeProvider>
       <div>
-        {CategoryPhotos()}
+        {CategoryPhotoGrid()}
         <OurPicksDivider>
-          <H1>Our Picks</H1>
+          <PhotoH1>Our Picks</PhotoH1>
         </OurPicksDivider>
-        <div className="our-picks">
-          <ImagesWrapper>
-            {
-              picksProducts.map((pick, index) => {
-                if (index < 3) {
-                  const photo = picksPhotos.find(p => +p.productId === +pick.id);
-                  return (
-                    <Link to={`/product/${pick.id}`} key={pick.id}>
-                      <p>{pick.title}</p>
-                      <img src={photo && photo.photoURL} alt={photo && photo.title} height="200" width="300" />
-                    </Link>
-                  );
-                }
-              })
-            }
-          </ImagesWrapper>
-        </div>
+        {PickedProuctGrid()}
       </div>
     </MuiThemeProvider>
   );
@@ -104,7 +140,7 @@ function Landing({ categories, picksProducts, picksPhotos }) {
 
 // Container
 const mapState = (state, ownProps) => {
-  const picksProducts = state.products;
+  const picksProducts = state.products.slice(0, 4);
   const picksPhotos = state.photos;
   return {
     categories: state.categories,

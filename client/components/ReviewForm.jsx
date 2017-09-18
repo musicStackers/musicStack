39f6cbbd6ca1b47436c19955b790705f69f4abab
jannncodes <janineagarcia@gmail.com>
2017-09-18 11:14:19 -0500

    photos for home page

--- client/components/ReviewForm.jsx
+++ client/components/ReviewForm.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
+import ActionStar from 'material-ui/svg-icons/toggle/star';
+import ActionStarBorder from 'material-ui/svg-icons/toggle/star-border';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import { setDescription } from '../store/reducers/review/description';
 import { setStar } from '../store/reducers/review/star';
 import { createReview } from '../store/reducers/review';
@@ -15,9 +20,25 @@ function ReviewForm({ description, star, setReviewDescription, setReviewStar, cr
     createThisReview(description, star, match.params.productId);
   }
 
+  const styles = {
+    block: {
+      maxWidth: 250,
+    },
+    radioButton: {
+      marginBottom: 16,
+    },
+  };
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
+        <RadioButtonGroup name="star-review" onChange={handleRadioChange} >
+          <RadioButtonGroup
+            value="1"
+            checkedIcon={<ActionStar />}
+            uncheckedIcon={<ActionStarBorder />}
+            style={styles.radioButton}
+        </RadioButtonGroup>
         <input name="stars" type="radio" value="1" id="star-1" checked={star === '1'} onChange={handleRadioChange} />
         <label htmlFor="star-1">1</label>
         <input name="stars" type="radio" value="2" id="star-2" checked={star === '2'} onChange={handleRadioChange} />

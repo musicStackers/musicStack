9b7681f9f6f2c4188a6c0ffb1fe68141da926833
Joon Kim <joon3575@gmail.com>
2017-09-20 08:01:55 -0500

    Fixed the alignment of Forte

--- client/components/reusableStyles.js
+++ client/components/reusableStyles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const TitleH1 = styled.h1`
   font-family: 'Pacifico', cursive;
   height: 100%;
-  padding: 17px;
+  padding: 20px;
   color: #1e88e5;
   font-size: 3em;
   text-align: center;
